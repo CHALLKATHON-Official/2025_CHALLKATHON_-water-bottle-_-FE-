adcc61738c3717e66b90a38fcac132a97fa9c17c
kth <0307rnxogh@daum.net>
2025-06-26 03:51:17 +0900

    .

--- WebSelf/pages/HomePage.tsx
+++ WebSelf/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-//import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ExtensionPage from './ExtensionPage';
 import NonExtensionPage from './NonExtensionPage';
 
