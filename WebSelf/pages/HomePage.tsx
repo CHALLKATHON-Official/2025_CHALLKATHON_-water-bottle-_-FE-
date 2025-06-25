88305dcdea43f7f7e114c5385cedeb0463270f96
kth <0307rnxogh@daum.net>
2025-06-26 04:13:08 +0900

    UUID 생성방식 수정

--- WebSelf/pages/HomePage.tsx
+++ WebSelf/pages/HomePage.tsx
@@ -3,12 +3,13 @@ import ExtensionPage from './ExtensionPage';
 import NonExtensionPage from './NonExtensionPage';
 
 const generateUUID = (): string => {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c: string) =>
-    (
-      Number(c) ^ (crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> (Number(c) / 4))
-    ).toString(16)
-  );
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = crypto.getRandomValues(new Uint8Array(1))[0] % 16;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 };
+
 
 const getOrCreateUserId = (): string => {
   let userId = localStorage.getItem("userId");
